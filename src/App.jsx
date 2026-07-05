d2849b8f754648f0a3fb76965e44d71e9d76fd8f
Sarvesh-88 <sk70689153@gmail.com>
2026-07-05 22:43:37 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import ProductDetails from "./pages/ProductDetails";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
